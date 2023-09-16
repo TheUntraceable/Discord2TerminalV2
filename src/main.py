7dc9096a525ed32f32db31466a833f645e72c5fc
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2023-09-16 12:02:37 +0100

    Entering logs file in w mode

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from rpc_client import Client
 
 logger = getLogger("rpc.client")
 
-handler = FileHandler(filename="rpc.log", encoding="utf-8")
+handler = FileHandler(filename="rpc.log", encoding="utf-8", mode="w")
 handler.setFormatter(Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 logger.setLevel(-100)
